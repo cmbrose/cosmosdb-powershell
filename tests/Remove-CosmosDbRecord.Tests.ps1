596f0d6d24247443ed1a02b0cddd450bec7ff141
Caleb Brose <{ID}+{username}@users.noreply.github.com>
2020-12-25 10:57:04 -0800

    Add more tests

---
+++ tests/Remove-CosmosDbRecord.Tests.ps1
@@ -0,0 +1,110 @@
+Get-Module cosmos-db | Remove-Module -Force
+Import-Module $PSScriptRoot\..\cosmos-db\cosmos-db.psm1 -Force
+
+InModuleScope cosmos-db {
+    Describe "Remove-CosmosDbRecord" {                    
+        BeforeAll {
+            . $PSScriptRoot\Utils.ps1    
+
+            $global:capturedNow = $null
+
+            $MOCK_SUB = "MOCK_SUB"
+            $MOCK_RG = "MOCK_RG"
+            $MOCK_DB = "MOCK_DB"
+            $MOCK_CONTAINER = "MOCK_CONTAINER"
+            $MOCK_COLLECTION = "MOCK_COLLECTION"
+            $MOCK_RECORD_ID = "MOCK_RECORD_ID"
+
+            $MOCK_AUTH_HEADER = "MockAuthHeader"
+
+            Function VerifyGetAuthHeader($ResourceGroup, $SubscriptionId, $Database, $verb, $resourceType, $resourceUrl, $now)
+            {
+                $ResourceGroup | Should -Be $MOCK_RG
+                $SubscriptionId | Should -Be $MOCK_SUB
+
+                $verb | Should -Be "delete"
+                $resourceType | Should -Be "docs"
+                $resourceUrl | Should -Be "dbs/$MOCK_CONTAINER/colls/$MOCK_COLLECTION/docs/$MOCK_RECORD_ID"
+            }
+
+            Function VerifyInvokeCosmosDbApiRequest($verb, $url, $body, $headers, $partitionKey=$MOCK_RECORD_ID)
+            {
+                $verb | Should -Be "delete"
+                $url | Should -Be "https://$MOCK_DB.documents.azure.com/dbs/$MOCK_CONTAINER/colls/$MOCK_COLLECTION/docs/$MOCK_RECORD_ID"        
+                $body | Should -Be $null
+                    
+                $global:capturedNow | Should -Not -Be $null
+
+                $expectedHeaders = Get-CommonHeaders -now $global:capturedNow -encodedAuthString $MOCK_AUTH_HEADER -PartitionKey $partitionKey
+            
+                AssertHashtablesEqual $expectedHeaders $headers
+            }
+
+            Mock Get-AuthorizationHeader {
+                param($ResourceGroup, $SubscriptionId, $Database, $verb, $resourceType, $resourceUrl, $now)
+        
+                VerifyGetAuthHeader $ResourceGroup $SubscriptionId $Database $verb $resourceType $resourceUrl $now | Out-Null
+        
+                $global:capturedNow = $now
+        
+                $MOCK_AUTH_HEADER
+            }
+        }
+
+        It "Sends correct request with default partition key" {    
+            $response = @{
+                StatusCode = 200;
+                Content = "{}"
+            }
+
+            Mock Invoke-CosmosDbApiRequest {
+                param($verb, $url, $body, $headers) 
+                
+                VerifyInvokeCosmosDbApiRequest $verb $url $body $headers | Out-Null
+        
+                $response
+            }
+
+            $result = Remove-CosmosDbRecord -ResourceGroup $MOCK_RG -SubscriptionId $MOCK_SUB -Database $MOCK_DB -Container $MOCK_CONTAINER -Collection $MOCK_COLLECTION -RecordId $MOCK_RECORD_ID
+
+            $result | Should -BeExactly $response
+        }
+
+        It "Sends correct request with custom partition key" {    
+            $response = @{
+                StatusCode = 200;
+                Content = "{}"
+            }
+
+            $partitionKey = "MOCK_PARTITION_KEY"
+
+            Mock Invoke-CosmosDbApiRequest {
+                param($verb, $url, $body, $headers) 
+                
+                VerifyInvokeCosmosDbApiRequest $verb $url $body $headers $partitionKey | Out-Null
+        
+                $response
+            }
+
+            $result = Remove-CosmosDbRecord -ResourceGroup $MOCK_RG -SubscriptionId $MOCK_SUB -Database $MOCK_DB -Container $MOCK_CONTAINER -Collection $MOCK_COLLECTION -RecordId $MOCK_RECORD_ID -PartitionKey $partitionKey
+
+            $result | Should -BeExactly $response
+        }
+
+        It "Should handle exceptions gracefully" {    
+            $response = [System.Net.HttpWebResponse]@{}
+
+            Mock Invoke-CosmosDbApiRequest {
+                param($verb, $url, $body, $headers) 
+                
+                VerifyInvokeCosmosDbApiRequest $verb $url $body $headers | Out-Null
+        
+                throw [System.Net.WebException]::new("", $null, [System.Net.WebExceptionStatus]::UnknownError, $response)
+            }
+
+            $result = Remove-CosmosDbRecord -ResourceGroup $MOCK_RG -SubscriptionId $MOCK_SUB -Database $MOCK_DB -Container $MOCK_CONTAINER -Collection $MOCK_COLLECTION -RecordId $MOCK_RECORD_ID
+
+            $result | Should -BeExactly $response
+        }
+    }
+}
--- tests/Utils.ps1
+++ tests/Utils.ps1
@@ -3,10 +3,31 @@ Function PSObjectToHashtable([parameter(ValueFromPipeline)]$psobject)
     $psobject.psobject.properties | % { $ht = @{} } { $ht[$_.Name] = $_.Value } { $ht }
 }
 
+Function AssertObjectsEqual($expected, $actual, [string]$path=$null)
+{
+    if ($expected -is [hashtable]) 
+    {
+        AssertHashtablesEqual $expected $actual $path
+    }
+    elseif ($expected -is [array])
+    {
+        AssertArraysEqual $expected $actual $path
+    }
+    else
+    {
+        AssertValuesEqual $expected $actual $path
+    } 
+}
+
+Function AssertValuesEqual($expected, $actual, [string]$path=$null)
+{
+    $actual | Should -Be $expected -Because "of expected value of $path"
+}
+
 Function AssertArraysEqual([array]$expected, $actual, [string]$path=$null)
 {
-    $actual -is [array] | Should -BeTrue -Because "expected array for path $path"
-    $actual.Count | Should -Be $expected.Count -Because "expected array for $path has that many items"
+    $actual -is [array] | Should -BeTrue -Because "the expected value for $path is an array"
+    $actual.Count | Should -Be $expected.Count -Because "the expected array for $path has that many items"
 
     0..($actual.Count - 1) | % {
         $a = $actual[$_]
@@ -14,18 +35,7 @@ Function AssertArraysEqual([array]$expected, $actual, [string]$path=$null)
 
         $currentPath = if ($path) { "$path[$_]" } else { "[$_]" }
 
-        if ($e -is [hashtable]) 
-        {
-            AssertHashtablesEqual $e $a $currentPath
-        }
-        elseif ($e -is [array])
-        {
-            AssertArraysEqual $e $a $currentPath
-        }
-        else
-        {
-            $a | Should -Be $e -Because "of expected value of $currentPath"
-        } 
+        AssertObjectsEqual $e $a $currentPath
     }
 }
 
@@ -35,14 +45,15 @@ Function AssertHashtablesEqual([hashtable]$expected, $actual, [string]$path=$nul
     {
         $actual = $actual | PSObjectToHashtable
     }
-    $actual -is [hashtable] | Should -BeTrue -Because "expected hashtable for path $path"
+
+    $actual -is [hashtable] | Should -BeTrue -Because "the expected value for $path is a hashtable"
 
     $actual.Keys | where { $null -ne $actual.$_ } | % {
-        $_ | Should -BeIn $expected.Keys -Because "key was in actual keys of path $path"
+        $_ | Should -BeIn $expected.Keys -Because "it was in actual keys of $path"
     }
 
     $expected.Keys | where { $null -ne $expected.$_ } | % {
-        $_ | Should -BeIn $actual.Keys -Because "key was in expected keys of path $path"
+        $_ | Should -BeIn $actual.Keys -Because "it was in expected keys of $path"
     }
 
     $actual.Keys | % { 
@@ -51,17 +62,6 @@ Function AssertHashtablesEqual([hashtable]$expected, $actual, [string]$path=$nul
 
         $currentPath = if ($path) { "$path.$_" } else { "$_" }
 
-        if ($e -is [hashtable]) 
-        {
-            AssertHashtablesEqual $e $a $currentPath
-        }
-        elseif ($e -is [array])
-        {
-            AssertArraysEqual $e $a $currentPath
-        }
-        else
-        {
-            $a | Should -Be $e -Because "of expected value of $currentPath"
-        }        
+        AssertObjectsEqual $e $a $currentPath      
     }
 }
